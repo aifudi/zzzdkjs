554766289d51725e0f92c19e6ad61afc1f6d40f2
aifudi <11532613@qq.com>
2017-11-17 16:12:58 +0800

    增加光纤记录函数的部分编辑完成

--- zzzdkjs/UserForm/AddrecForm.cs
+++ zzzdkjs/UserForm/AddrecForm.cs
@@ -25,7 +25,22 @@ namespace zzzdkjs.UserForm
         /// <param name="e"></param>
         private void button1_Click(object sender, System.EventArgs e)
         {
+            FiberRecord rec = GetEditedRec();
             AddFiberRec(fiberrec);
+        }
+
+        /// <summary>
+        /// 得到新增的光纤记录值
+        /// </summary>
+        /// <returns></returns>
+        private FiberRecord GetEditedRec()
+        {
+            FiberRecord rec = new FiberRecord();
+            rec.FiberPigtail = textBoxFiberTail.Text;
+            rec.TeleOperator = comboBoxOperator.Items[comboBoxOperator.SelectedIndex].ToString();
+
+            return rec;
+
         }
     }
 }
